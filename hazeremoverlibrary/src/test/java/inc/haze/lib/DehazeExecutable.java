b095322774991b4a78908dc4b7f5edec047348af
zyulyaev <nikzyul@ya.ru>
2016-04-17 10:19:19 +0300

    examples

--- hazeremoverlibrary/src/test/java/inc/haze/lib/DehazeExecutable.java
+++ hazeremoverlibrary/src/test/java/inc/haze/lib/DehazeExecutable.java
@@ -2,29 +2,35 @@ package inc.haze.lib;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Created by nikita on 16.04.16.
  */
 public class DehazeExecutable {
     public static void main(String[] args) throws IOException {
-        BufferedImage image = ImageIO.read(DehazeExecutable.class.getResourceAsStream("/forest.png"));
-        int height = image.getHeight();
-        int width = image.getWidth();
-        int[] pixels = image.getRGB(0, 0, width, height, null, 0, width);
-        long start = System.currentTimeMillis();
-        DehazeResult dehaze = new HazeRemover(new GuidedFilter(), 2000, 2000).dehaze(pixels, height, width);
-        long end = System.currentTimeMillis();
-        System.out.println(end - start);
+        HazeRemover remover = new HazeRemover(new GuidedFilter(), 3000, 3000);
 
-        BufferedImage dehazed = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        dehazed.setRGB(0, 0, width, height, dehaze.getResult(), 0, width);
-        ImageIO.write(dehazed, "PNG", new File("test-output.png"));
+        Files.newDirectoryStream(Paths.get("examples"), "*.{jpg,png}").forEach(path -> {
+            try {
+                BufferedImage image = ImageIO.read(path.toFile());
+                int height = image.getHeight();
+                int width = image.getWidth();
+                int[] pixels = image.getRGB(0, 0, width, height, null, 0, width);
+                DehazeResult dehaze = remover.dehaze(pixels, height, width);
 
-        BufferedImage depth = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        depth.setRGB(0, 0, width, height, dehaze.getDepth(), 0, width);
-        ImageIO.write(depth, "PNG", new File("test-depth.png"));
+                BufferedImage dehazed = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+                dehazed.setRGB(0, 0, width, height, dehaze.getResult(), 0, width);
+                ImageIO.write(dehazed, "PNG", path.resolveSibling(path.getFileName() + "-output.png").toFile());
+
+                BufferedImage depth = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+                depth.setRGB(0, 0, width, height, dehaze.getDepth(), 0, width);
+                ImageIO.write(depth, "PNG", path.resolveSibling(path.getFileName() + "-depth.png").toFile());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 }
